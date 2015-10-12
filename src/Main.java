9e3f70ebd1412d2a2622e61e3419d4a2eb7fd41b
maciej <several27@icloud.com>
2015-10-12 23:44:49 +0100

    Tutorial 2 - Finished

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,19 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class Main extends Application
+public class Main extends Application implements EventHandler<ActionEvent>
 {
 	Button button;
 
-    public static void main(String[] args)
-    {
-        launch(args);
-    }
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception
@@ -20,6 +22,7 @@ public class Main extends Application
 
 		button = new Button();
 		button.setText("Click me");
+		button.setOnAction(this);
 
 		StackPane layout = new StackPane();
 		layout.getChildren().add(button);
@@ -28,5 +31,14 @@ public class Main extends Application
 
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+
+	@Override
+	public void handle(ActionEvent event)
+	{
+		if (event.getSource() == button)
+		{
+			System.out.println("I love when you touch me");
+		}
 	}
 }
