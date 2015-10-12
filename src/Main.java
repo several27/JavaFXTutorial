3f09b72ed1a9a13b93649d9d072dd9866ecdb821
maciej <several27@icloud.com>
2015-10-12 23:28:25 +0100

    Tutorial 1 - Finished

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class Main extends Application
+{
+	Button button;
+
+    public static void main(String[] args)
+    {
+        launch(args);
     }
+
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		primaryStage.setTitle("Title of the Window");
+
+		button = new Button();
+		button.setText("Click me");
+
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+
+		Scene scene = new Scene(layout, 300, 250);
+
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
