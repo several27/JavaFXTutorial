52501b17cb1fc2e52d91dff00cbf79589c9c4586
maciej <several27@icloud.com>
2015-10-13 09:25:37 +0100

    Tutorial 7 - Finished

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -26,11 +27,13 @@ public class Main extends Application
 		this.primaryStage = primaryStage;
 		this.primaryStage.setTitle("Title");
 
-		button = new Button("Click me");
-		button.setOnAction(e -> {
-			boolean result = ConfirmBox.display("Title", "Are u sure ?");
-			System.out.println(result);
+		this.primaryStage.setOnCloseRequest(e -> {
+			e.consume(); // i'm going to care about closing the program
+			closeProgram();
 		});
+
+		button = new Button("Cloes window");
+		button.setOnAction(e -> closeProgram());
 
 		StackPane stackPane = new StackPane();
 		stackPane.getChildren().add(button);
@@ -38,5 +41,14 @@ public class Main extends Application
 		Scene scene = new Scene(stackPane, 300, 250);
 		this.primaryStage.setScene(scene);
 		this.primaryStage.show();
+	}
+
+	public void closeProgram()
+	{
+		System.out.println("Before program close");
+
+		Boolean answer = ConfirmBox.display("Closing program", "Are you sure, that you want to close this window?");
+		if (answer)
+			primaryStage.close();
 	}
 }
